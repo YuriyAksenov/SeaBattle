0d85910688f1eab8822fea10cb689fbf6e890a8f
Yurii Aksenov <Yura290610@yandex.ru>
2017-04-15 23:30:24 +0300

    Add Base Classes, which determines main movels

---
+++ SeaBattle/Model/BaseField.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle.Model
+{
+    public abstract class BaseField
+    {
+        FieldCell[,] Cells;
+    }
+
+
+    enum FieldCell : byte
+    {
+        Empty = 0,
+        Full = 1,
+        Hit = 2,
+        Past = 3
+    }
+}
---
+++ SeaBattle/Model/BaseShip.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle.Model
+{
+    /// <summary>
+    /// Provides the instance of main unit
+    /// </summary>
+    public abstract class BaseShip
+    {
+        public int HorizontalStartCell { get; }
+        public int VerticalStartCell { get; }
+        public Direction ShipDirection { get; }
+        public int ShipLength { get; }
+        public bool[] ShipCells { get; set; }
+
+        public BaseShip(int horizontalStartCell, int verticalStartCell, Direction shipDirection, int shipLength)
+        {
+            this.HorizontalStartCell = horizontalStartCell;
+            this.VerticalStartCell = verticalStartCell;
+            this.ShipDirection = shipDirection;
+            this.ShipLength = shipLength;
+        }
+    }
+
+    public enum Direction : byte
+    {
+        Horizontal = 0,
+        Vertical = 1
+    }
+
+}
